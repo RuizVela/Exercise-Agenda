d037b2ffa60ec14bce1d214b862eb348333ecfc2
Raúl <ruizvela271@gmail.com>
2020-04-07 16:10:36 +0200

    Clase Contacto creada con atributos.

---
+++ Agenda con Archivos/Contacto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_con_Archivos
+{
+    public class Contacto
+    {
+        private string _nombre;
+        private string _apellidos;
+        private int _telefono;
+        private string _localidad;
+    }
+}
