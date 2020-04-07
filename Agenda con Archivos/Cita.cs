410b70704a8a28dba69145eca8782ee614292510
Raúl <ruizvela271@gmail.com>
2020-04-07 12:22:36 +0200

    Created Class Cita with attributes

---
+++ Agenda con Archivos/Cita.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_con_Archivos
+{
+    public class Cita
+    {
+        private readonly DateTime _fecha;
+        private readonly string _descripcion;
+        private readonly string _nombre;
+        public Cita(DateTime fecha, string descripcion, string nombre)
+        {
+            _fecha = fecha;
+            _descripcion = descripcion;
+            _nombre = nombre;
+        }
+    }
+}
