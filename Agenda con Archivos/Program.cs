30ab9888acb7adabcc0b424b29590b6040e04134
Raúl <ruizvela271@gmail.com>
2020-04-07 12:12:37 +0200

    First Commit

---
+++ Agenda con Archivos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda_con_Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t1 - Añadir cita");
+            Console.WriteLine("\t2 - Añadir contacto");
+            Console.WriteLine("\t3 - Consultar cita");
+            Console.WriteLine("\t4 - Buscar contacto");
+            Console.WriteLine("\t0 - Salir");
+
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    //
+                    break;
+                case "2":
+                    //
+                    break;
+                case "3":
+                    //
+                    break;
+                case "4":
+                    //
+                    break;
+                case "0":
+                    //
+                    break;
+            }
+        }
+    }
+}
