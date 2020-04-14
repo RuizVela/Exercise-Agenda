22cf8315503d65d182daa6d9c5aa9b0d9e8cb6ae
Raúl <ruizvela271@gmail.com>
2020-04-14 12:14:38 +0200

    Metodo Search en DateSearcher para poder buscar citas por fecha.

--- Agenda con Archivos/Models/DateSearcher.cs
+++ Agenda con Archivos/Models/DateSearcher.cs
@@ -1,5 +1,6 @@
 ﻿using Agenda_con_Archivos.Services;
 using System;
+using System.IO;
 
 namespace Agenda_con_Archivos.Models
 {
@@ -7,7 +8,47 @@ namespace Agenda_con_Archivos.Models
     {
         public void Search()
         {
-            throw new NotImplementedException();
+            var date = InsertDate();
+            DateSearch(date);
+            Console.ReadLine();
+        }
+        private DateTime InsertDate()
+        {
+            bool correctDate = false;
+            DateTime date = new DateTime();
+            while (!correctDate)
+            {
+                Console.WriteLine("Inserte la fecha de la cita. Ejemplo: 21/05/2001");
+                correctDate = DateTime.TryParse(Console.ReadLine(), out date);
+            }
+            return date;
+        }
+        private void DateSearch(DateTime day)
+        {
+                bool dateExists = false;
+            try
+            {
+                var allDates = File.ReadAllLines("Agenda.txt");
+                foreach (string date in allDates)
+                {
+                    if (date.Contains(day.ToString("dd/MM/yyyy")))
+                    {
+                    Console.WriteLine(date);
+                    dateExists = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //La excepción no impide que el programa siga funcionando.
+            }
+            finally
+            {
+                if (!dateExists)
+            {
+                Console.WriteLine("No existen citas para ese día.");
+            }
+            }
         }
     }
 }
